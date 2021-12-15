e88a163d1873d9bc7a0c125fbdbfc6b0786ff69b
Kelly Stone <kellystone916@gmail.com>
2021-12-15 12:34:23 -0800

    adding files

---
+++ src/components/AnimalFacts.vue
@@ -0,0 +1,13 @@
+<template>
+<main>
+    <h1> Interesting Animal Facts </h1>
+    <h4>Cow:</h4>
+    <p> A cow has 32 teeth. </p>
+    <h4>Cat:</h4>
+    <p> Cats sleep an average of 13-14 hours a day. </p>
+    <h4>Duck:</h4>
+    <p> A wild duck can live 20 years or more. </p>
+    <h4>Dog:</h4>
+    <p> Dogs have a sense of smell 40x better than humans. </p>
+</main>
+</template>
